c15dd119936eb3ad6a32f9ec98dade78513eb4f6
Nana Kay <christian.osei-bonsu@meltwater.org>
2013-03-04 19:10:17 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterOnRails::Application.config.secret_token = 'a2d7b6830e3d75b3d09f20c09924b60504c4360f61ee71c987272087e145d6a51a55d9a67114dc00faa9629601026eaac30ceeb2a6f2d12c0a033862a79a78bb'
